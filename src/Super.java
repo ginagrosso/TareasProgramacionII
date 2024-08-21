3b81d4ceae23b9aaeb5b4a319fe1562d86e75586
ginagrosso <ginagrosso01@gmail.com>
2024-08-21 09:27:32 -0300

    Update Super.java

--- src/Super.java
+++ src/Super.java
@@ -1,20 +1,3 @@
-/**Imagina que estás creando un juego de supermercado en tu computadora. Necesitamos que escribas un programa en Java para que
- * todo funcione como en la vida real.
-
- ¿Cómo lo haremos?
-
- Humane: Tenemos una clase base para representar a cualquier persona (empleados y clientes).
- Empleados: Son humanes que trabajan en las cajas y tienen un sueldo.
- Cajas: Son los lugares donde se pagan las compras y están atendidas por un empleado o empleada.
- Clientes: También son humanes, pero además sabemos si son mayoristas o no (para darles descuentos).
-
- Tu misión:
-
- Crea los productos: Cada producto tiene un nombre, precio y cantidad.
- Registra las compras: Cuando un cliente compra, se crea una transacción. Esta transacción guarda qué productos compró, cuánto pagó y en qué caja lo hizo.
- Simula una compra: Haz que un cliente vaya a una caja, compre varios productos y pague. ¡Muestra en pantalla
- todo lo que compró y cuánto pagó!*/
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
